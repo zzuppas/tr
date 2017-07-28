67ab0aa94a98983223f51e770096bc74c2ff00ce
zzuppas <zzuppas@gmail.com>
2017-07-28 16:19:22 -0600

    Allows to choose a hat

--- saved_items.py
+++ saved_items.py
@@ -16,7 +16,7 @@ class Database:
         self._db_filename = filename
         exists = os.path.isfile(filename)
         self._db = sqlite3.connect(filename)
-        self._db.isolation_level = "IMMEDIATE"
+        #self._db.isolation_level = "IMMEDIATE"
         self._db.row_factory = sqlite3.Row
         if not exists:
             self.initialize()
@@ -46,18 +46,36 @@ class SavedItems(Database):
         # NULL INTEGER REAL TEXT BLOB
         # https://sqlite.org/datatype3.html
         cur = self._db.cursor()
-        cur.execute("CREATE TABLE hats (id INTEGER PRIMARY KEY, type TEXT, name TEXT, value TEXT)")
-        cur.execute("INSERT INTO hats(type, name, value) VALUES('top hat', 'ralph', 'big red top hat')")
-        cur.execute("INSERT INTO hats(type, name, value) VALUES('fedora', 'buster', 'small blue fedora')")
+        cur.execute("CREATE TABLE hats (id INTEGER PRIMARY KEY, type TEXT, name TEXT, brief TEXT)")
+        cur.execute("INSERT INTO hats(type, name, brief) VALUES('top hat', 'ralph', 'big red top hat')")
+        cur.execute("INSERT INTO hats(type, name, brief) VALUES('fedora', 'buster', 'small blue fedora')")
         self._db.commit()
 
+    def list_hats(self):
+        cursor = self._db.execute('select * from {} order by id'.format('hats'))
+        hats = []
+        for row in cursor:
+            hats.append('The {} named {} is a {}.'.format(row['type'], row['name'].title(), row['brief']))
+        return hats
 
 def test():
     items = SavedItems(filename=ONE_AND_ONLY_DB)
-    # for hat in items.hats():
-    #     print(hat)
-    #item.close()
-    #input('done')
+    print("There are various hats to choose from.")
+    hat_list = items.list_hats()
+    for index, hat in enumerate(hat_list):
+        print("{}. ".format(index + 1) + hat)
+    choice = None
+    while True:
+        try:
+            choice = int(input('Which one would you like? >')) - 1
+        except ValueError:
+            print("Make choice by number")
+            continue
+        if choice < 0 or choice >= len(hat_list):  #TODO: this is not robust because we don't know all valid ids
+            print("That is not a valid option")
+            continue
+        break
+    print("Congrats on choosing \"" + hat_list[choice] + "\"")
 
 if __name__ == "__main__":
     test()
