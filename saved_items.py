8fe9cb43ffb1fa7d2a56a49c8d088f2c91e29c8c
zzuppas <zzuppas@gmail.com>
2017-07-28 15:29:17 -0600

    wip

--- saved_items.py
+++ saved_items.py
@@ -1,28 +1,63 @@
 import sqlite3
+import os
 
-conn = None
+ONE_AND_ONLY_DB = 'tr.db'
 
-def initialize():
-    global conn;
-    conn = sqlite3.connect("tr.db")
-    cur = conn.cursor()
-    result = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='clothes';")
-    if result.fetchone() is None:
-        # Storage classes
+class Database:
+    def __init__(self, **kwargs):
+        self.db_filename = kwargs.get('filename')
+
+    @property
+    def db_filename(self):
+        return self._db_filename
+
+    @db_filename.setter
+    def db_filename(self, filename):
+        self._db_filename = filename
+        exists = os.path.isfile(filename)
+        self._db = sqlite3.connect(filename)
+        self._db.isolation_level = "IMMEDIATE"
+        self._db.row_factory = sqlite3.Row
+        if not exists:
+            self.initialize()
+
+    @db_filename.deleter
+    def db_filename(self):
+        self.close()
+
+    def close(self):
+        self._db.close()
+        del self._db_filename
+
+    def initialize(self):
+        raise NotImplementedError("You can't triple stamp a double stamp")
+
+    def sql_exec(self, msg, *params):
+        self._db.execute(msg, params)
+        self._db.commit()
+
+
+class SavedItems(Database):
+    def __init__(self, **kwargs):
+        Database.__init__(self, **kwargs)
+
+    def initialize(self):
+        # Note: Storage classes are
         # NULL INTEGER REAL TEXT BLOB
         # https://sqlite.org/datatype3.html
-        #result = cur.execute("CREATE TABLE clothes (id INTEGER PRIMARY KEY)")
-        cur.execute("CREATE TABLE clothes (id INTEGER PRIMARY KEY, type TEXT, name TEXT, value TEXT)")
-        cur.execute("INSERT INTO clothes(type, name, value) VALUES('hat', 'top hat', 'big red top hat')")
-        cur.execute("INSERT INTO clothes(type, name, value) VALUES('hat', 'fedora', 'small blue fedora')")
-    else:
-        result = cur.execute("SELECT * FROM clothes;")
-        for x in result:
-            print("Clothing is " + str(x))
+        cur = self._db.cursor()
+        cur.execute("CREATE TABLE hats (id INTEGER PRIMARY KEY, type TEXT, name TEXT, value TEXT)")
+        cur.execute("INSERT INTO hats(type, name, value) VALUES('top hat', 'ralph', 'big red top hat')")
+        cur.execute("INSERT INTO hats(type, name, value) VALUES('fedora', 'buster', 'small blue fedora')")
+        self._db.commit()
 
 
-def close():
-    if conn is not None:
-        print("closing")
-        conn.commit()
-        conn.close()
+def test():
+    items = SavedItems(filename=ONE_AND_ONLY_DB)
+    # for hat in items.hats():
+    #     print(hat)
+    #item.close()
+    #input('done')
+
+if __name__ == "__main__":
+    test()
